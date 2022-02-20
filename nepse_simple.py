3b83d9f4ab23717e0f8cfe49b563589637cfea0b
Sumit Yadav <58735748+rockerritesh@users.noreply.github.com>
2022-02-20 19:20:35 +0545

    index updated

--- nepse_simple.py
+++ nepse_simple.py
@@ -83,7 +83,11 @@ if (localStorage.getItem('theme') === 'dark') {
 	 
     </style>
 
-<body>"""
+<body>
+<iframe title="Nepse Today  Data" aria-label="Table" id="datawrapper-chart-yAfsH" src="https://datawrapper.dwcdn.net/yAfsH/1/" scrolling="no" frameborder="0" style="width: 0; min-width: 100% !important; border: none;" height="1475"></iframe><script type="text/javascript">!function(){"use strict";window.addEventListener("message",(function(e){if(void 0!==e.data["datawrapper-height"]){var t=document.querySelectorAll("iframe");for(var a in e.data["datawrapper-height"])for(var r=0;r<t.length;r++){if(t[r].contentWindow===e.source)t[r].style.height=e.data["datawrapper-height"][a]+"px"}}}))}();
+</script>
+
+"""
 
 
 import requests
@@ -201,3 +205,9 @@ html = "<html><head></head><body>" + str(category[2]) + "</body> </html>"
 with open("marketsummary.html", "w") as output:
     output.write(html)
 """
+df=pd.read_json('https://nepalstockapi.herokuapp.com/allprice.php')
+df1 = df.sort_values(by=["nooftransaction"])
+html=df1.to_html()
+html = asadas + html + "</body> </html>"
+with open("index.html", "w") as output:
+    output.write(html)
