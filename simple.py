0ab77442ee3cdd7eec23cacc4ddf67838f8f011f
Sumit Yadav <58735748+rockerritesh@users.noreply.github.com>
2023-04-30 21:11:49 +0545

    Update simple.py
    Graph_error

--- simple.py
+++ simple.py
@@ -27,7 +27,7 @@ from telegram import InputFile
 # bot = telegram.Bot(token=BOT_TOKEN)
 # In[143]:
 
-
+'''
 url2 = "https://www.nepalipaisa.com/Indices.aspx"
 html2 = requests.get(url2, verify=False).content
 df_list2 = pd.read_html(html2)
@@ -67,6 +67,8 @@ plt.savefig("graph.png")
 # Send the image to the chat using the chat ID
 # with open("graph.png", "rb") as f:
 #    bot.send_photo(chat_id=CHAT_ID, photo=InputFile(f))
+
+'''
 
 # FOR STOCK
 
