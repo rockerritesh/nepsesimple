7ee3d6828c2cba34ed316655a5aab7a6396c1765
Sumit Yadav <58735748+rockerritesh@users.noreply.github.com>
2023-04-30 22:03:17 +0545

    Update simple.py

--- simple.py
+++ simple.py
@@ -10,11 +10,13 @@ with open("html.txt", "r") as f:
 import requests
 import pandas as pd
 import numpy as np
+from sklearn.linear_model import LinearRegression
 
 # import openpyxl
 import matplotlib.pyplot as plt
 import bs4
 import html5lib
+
 
 # ----MY----
 # BOT_TOKEN = ""
@@ -32,7 +34,7 @@ from telegram import InputFile
 url = "https://merolagani.com/Indices.aspx"
 df = pd.read_html(url)[0]
 df = df.iloc[::-1].reset_index(drop=True)
-print(df)
+
 # Create X and Y arrays for regression analysis
 y = np.array(df["Index Value"])
 x = np.linspace(1, len(y), len(y)).reshape(-1, 1)
@@ -52,7 +54,22 @@ date_str = date.isoformat(today)
 plt.figure(figsize=(16, 8))
 plt.plot(df["Date (AD)"], df["Index Value"], "bo-", label="Index Value")
 plt.plot(date_str, next_index, "ro", label="Predicted Next Index Value")
+
+# Add annotations
+plt.annotate(f"Predicted: {next_index:.2f}", xy=(date_str, next_index),
+             xytext=(-50, 30), textcoords="offset points",
+             arrowprops=dict(arrowstyle="->", color="red"))
+plt.annotate(f"Regression Line: {reg.coef_[0]:.2f}x + {reg.intercept_:.2f}", 
+             xy=(0.05, 0.95), xycoords="axes fraction",
+             fontsize=12, ha="left", va="top")
+
+# Add regression line
 plt.plot(df["Date (AD)"], reg.predict(x), "g--", label="Regression Line")
+
+# Format x-axis
+plt.xticks(rotation=45, ha="right")
+plt.gca().xaxis.set_major_locator(plt.MaxNLocator(10))
+
 plt.xlabel("Date")
 plt.ylabel("Index Value")
 plt.title("Nepal Stock Exchange NEPSE Index Value")
