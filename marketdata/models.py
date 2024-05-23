060cf7d0d168476c72bfe8b261370a2c6b46c54f
rockerritesh <rockerritesh4@gmail.com>
2024-05-23 08:41:44 +0545

    fix: :art: update model
    This will only fetch data one time, from other server, and store in database.

--- marketdata/models.py
+++ marketdata/models.py
@@ -1,14 +1,39 @@
 from django.db import models
+from picklefield.fields import PickledObjectField
 
-# Create your models here.
 class StockData(models.Model):
-    # date = models.DateField()
-    volume = models.IntegerField()
-    turnover = models.FloatField()
-    transaction = models.IntegerField()
+    date = models.DateField(auto_now_add=True)
+    total_stocks = models.IntegerField()
+    total_market_cap = models.DecimalField(max_digits=20, decimal_places=2, default=0)
+    total_traded_value = models.DecimalField(max_digits=20, decimal_places=2, default=0)
+    total_traded_quantity = models.DecimalField(max_digits=20, decimal_places=2, default=0)
+    data = PickledObjectField()
+    columns = PickledObjectField()
+
+class IPOEvents(models.Model):
+    date = models.DateField(auto_now_add=True)
+    message = models.TextField()
+
+class StockDetails(models.Model):
+    date = models.DateField(auto_now_add=True)
     symbol = models.CharField(max_length=10)
+    conf = models.FloatField()
+    open = models.FloatField()
     high = models.FloatField()
     low = models.FloatField()
     close = models.FloatField()
-    open = models.FloatField()
-    
+    vwap = models.FloatField()
+    vol = models.IntegerField()
+    prev_close = models.FloatField()
+    turnover = models.FloatField()
+    trans = models.IntegerField()
+    diff = models.FloatField()
+    range = models.FloatField()
+    diff_percentage = models.FloatField()
+    range_percentage = models.FloatField()
+    vwap_percentage = models.FloatField()
+    days_120 = models.FloatField()
+    days_180 = models.FloatField()
+    weeks_high = models.FloatField()
+    weeks_low = models.FloatField()
+    percentage_change = models.FloatField()
--- marketdata/views.py
+++ marketdata/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from .utils.utils import csv_json, get_ipo_message, get_stock_details
 from ninja import NinjaAPI
-import logging
+# import logging
 from django.contrib.auth.decorators import login_required
+from .models import StockData, IPOEvents
+from datetime import datetime
+import numpy as np
+import pandas as pd
 
 # logger = logging.getLogger(__name__)
 
@@ -11,36 +15,95 @@ api = NinjaAPI()
 
 @api.get("/stock_market")
 def stock_market_view(request):
-    df = csv_json()
-    total_stocks = int(len(df))
-    total_market_cap = float(df['Turnover'].sum())
-    total_traded_value = int(df['Trans.'].sum())
-    total_traded_quantity = int(df['Vol'].sum())
+    
+    # Get's Today date
+    today = datetime.today().date()
+    
+    # Retrieve all objects for today's date
+    stock_data_objects = StockData.objects.filter(date=today)
+    
+    if stock_data_objects.exists():
+        # If data already exists for today, use the first object
+        stock_data = stock_data_objects.first()
+        total_stocks = stock_data.total_stocks
+        total_market_cap = stock_data.total_market_cap
+        total_traded_value = stock_data.total_traded_value
+        total_traded_quantity = stock_data.total_traded_quantity
+    else:
+        # Calculate new data if no data exists for today
+        df = csv_json()
+        total_stocks = int(len(df))
+        total_market_cap = float(df['Turnover'].sum())
+        total_traded_value = float(df['Trans.'].sum())  # Corrected to float
+        total_traded_quantity = float(df['Vol'].sum())  # Corrected to float
+        data = np.array(df)
+        columns = df.columns.tolist()
+        
+        # Create a new instance of StockMarket model
+        stock_data = StockData(
+            date=today,
+            total_stocks=total_stocks,
+            total_market_cap=total_market_cap,
+            total_traded_value=total_traded_value,
+            total_traded_quantity=total_traded_quantity,
+            data = data,
+            columns = columns
+        )
+        
+        # Save the instance to the database
+        stock_data.save()
+    
     context = {
         'total_stocks': total_stocks,
         'total_market_cap': total_market_cap,
         'total_traded_value': total_traded_value,
         'total_traded_quantity': total_traded_quantity
     }
-    # logger.info(f"Total Stocks: {total_stocks}")
-    # logger.info(f"Total Market Capitalization: {total_market_cap}")
-    # logger.info(f"Total Traded Value: {total_traded_value}")
-    # logger.info(f"Total Traded Quantity: {total_traded_quantity}")
     
     return context
 
 @api.get("/ipo_events")
 def ipo_events_view(request):
-    url = "https://www.sharesansar.com/?show=home"
-    message = get_ipo_message(url)
+    
+    # Get's Today date
+    today = datetime.today().date()
+    
+    # Check if the data is already saved in the database
+    if not IPOEvents.objects.filter(date=today).exists():
+        url = "https://www.sharesansar.com/?show=home"
+        message = get_ipo_message(url)
+    
+        # create a new instance of IPOEvents model
+        ipo_event = IPOEvents(
+            date=today,
+            message=message
+        )
+        
+        # save the instance to the database
+        ipo_event.save()
+    else:
+        # If data already exists for today, retrieve it from the database
+        ipo_event = IPOEvents.objects.get(date=today)
+        message = ipo_event.message
+    
     return message
+
 
 @api.get("/stock_market/{symbol}/today")
 def stock_market_symbol_view(request, symbol: str):
-    df = csv_json()
+    # Get's Today date
+    today = datetime.today().date()
+    
+    # Retrieve all objects for today's date
+    stock_data_objects = StockData.objects.filter(date=today)
+    stock_data = stock_data_objects.first()
+    data = stock_data.data
+    columns = stock_data.columns
+    df = pd.DataFrame(data, columns=columns)
+    
     stock = df[df['Symbol'] == symbol]
     stock = stock.to_dict(orient='records')
-    return stock
+    return stock[0]
 
 @api.get("/stock_market/{symbol}/history")
 def stock_market_symbol_history_view(request, symbol: str):
